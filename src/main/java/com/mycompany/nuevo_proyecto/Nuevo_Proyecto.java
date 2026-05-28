8eeaaf6950ceb865774ccba0181e2cca85f77b4b
leonardo <ltalavera9222@gmail.com>
2026-05-28 18:48:17 -0500

    se creo un generador de id manual

--- src/main/java/com/mycompany/nuevo_proyecto/Nuevo_Proyecto.java
+++ src/main/java/com/mycompany/nuevo_proyecto/Nuevo_Proyecto.java
@@ -14,55 +14,106 @@ public class Nuevo_Proyecto {
         int opcion;
         
         do{
+            //usuario ingresa valores
              System.out.println("===== SISTEMA DE FARMACIA =====");
              System.out.println("1. Agregar producto");
              System.out.println("2. Mostrar productos");
-             System.out.println("3. Salir");
+             System.out.println("3. Buscar producto");
+             System.out.println("4. salir");
              System.out.print("Seleccione una opción: ");
              opcion = teclado.nextInt();
          
             switch(opcion){
-                
+                //se agragan valores
                 case 1:
+                    
+                    //pide id
+                    System.out.print("ID del producto: ");
+                    int id = teclado.nextInt();
                     teclado.nextLine();
-
+                    boolean idExiste = false;
+                    
+                    //empieza a buscar producto por producto 
+                    for(Product p : productos){
+                         //si el id del producto es igual al id ingresado
+                        if(p.getId() == id){
+                            //encontro a uno igual entonces.....
+                        idExiste = true;
+                        }
+                    }
+                    //se ingresa a este if
+                    if(idExiste){
+                        System.out.println("Ese id ya existe");
+                        
+                    }
+                    //sino ingresa al else
+                    else{
+                      //pide nombre
                     System.out.print("Nombre del producto: ");
                     String nombre = teclado.nextLine();
-
+                     //pide precio
                     System.out.print("Precio: ");
                     double precio = teclado.nextDouble();
-
+                     //pide stock
                     System.out.print("Stock: ");
                     int stock = teclado.nextInt();
-
-                    Product nuevoProducto = new Product(nombre, precio, stock);
-
+                    teclado.nextLine();
+                    //guarda en el constructor
+                    Product nuevoProducto = new Product(id,nombre, precio, stock);
+                     //se crea nuevo valor 
                     productos.add(nuevoProducto);
-
-                    System.out.println("¡Producto agregado correctamente!");
-
-                    break;
                     
+                    System.out.println("¡Producto agregado correctamente!");
+                    }
+                    
+                    break;
+                //muestra la lista de productros guardados  
                 case 2:
                     System.out.println("===== LISTA DE PRODUCTOS =====");
                      for (Product p : productos) {
-                         
+                         //muestra los productos guardados
                          p.mostrarProducto();
-                         
+                          //separador de lineas
                          System.out.println("----------------");                   
                      
                      }
                        break;
-                
+                //busca la id de los productos guardados       
                 case 3:
+                     //pide id a buscar
+                    System.out.println("Ingrese ID a buscar: ");
+                    int buscarId = teclado.nextInt();
+                     //crea un valor booleano para buscar si existe el valor solicitado
+                    boolean encontrado = false;
+                     //empieza a buscar producto por producto 
+                    for(Product p: productos){
+                         //si el id del producto es igual al id solicitado
+                        if(p.getId()==buscarId){
+                            
+                             //muestra el producot
+                        p.mostrarProducto();
+                         //para la accion
+                        encontrado = true;
+                        }
+                    }
+                     //si el id no existe 
+                    if(!encontrado){
+                    
+                        System.out.println("Producto no encontrado");
+                        
+                    }
+                    break;
+                //cierra el programa 
+                case 4:
                     System.out.println("Cerrando sistema...");
                     break;
                     
                 default:
-                        
+                         //en caso de que pongan un 5 a mas
                     System.out.println("Opción inválida");
             }
-        }while (opcion!=3);
+        //el final del programa
+        }while (opcion!=4);
         
     }
 }
