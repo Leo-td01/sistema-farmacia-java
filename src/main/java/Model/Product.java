c222e892f6304fb068366fa0791eb0f77361efee
leonardo <ltalavera9222@gmail.com>
2026-05-28 19:34:19 -0500

    El id es automatico

--- src/main/java/Model/Product.java
+++ src/main/java/Model/Product.java
@@ -3,19 +3,30 @@ package Model;
 
 
 public class Product {
-      private final String nombre;
+     //creando valores de la madre
+    private final int id;
+    private final String nombre;
     private final double precio;
     private final int stock;
-
-    public Product(String nombre, double precio, int stock) {
+     //creaando el constructor
+    public Product(int id, String nombre, double precio, int stock) {
+        this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.stock = stock;
     }
-
+     //creando lo el mostrar 
     public void mostrarProducto() {
+        
+        System.out.println("===== PRODUCTO =====");
+        System.out.println("ID: " + id);
         System.out.println("Producto: " + nombre);
         System.out.println("Precio: S/" + precio);
         System.out.println("Stock: " + stock);
     }
+     //termina el get con id para que me4 pueda mostrar el id 
+    public int getId(){
+        return id;
+    }  
+    
 }
